4b63d135897804674119838fedbb5a4b1f94286b
Chandan-kr-dev <chandanramgar@gmail.com>
2024-07-11 01:05:29 +0530

    divide two integers

---
+++ DivideTwoIntegers/ques.java
@@ -0,0 +1,21 @@
+package DivideTwoIntegers;
+
+public class ques {
+    static int divide(int d,int a){
+        int rem=d-a;
+        int quo=1;
+        while(rem>a){
+            rem-=a;
+            quo++;
+        }
+        return quo;
+    }
+    public static void main(String[] args) {
+        int divi=10;
+        int dis=3;
+        int ans=divide(divi, dis);
+        System.out.println(ans);
+
+    }
+    
+}
